24bd22f1adbcedbdefc98b2b256a8b83d8b429ef
SupraConstruct <83255856+beefrolls@users.noreply.github.com>
2023-09-04 19:10:31 +0800

    Added html, css and code along with pseudocode

---
+++ code.js
@@ -0,0 +1,15 @@
+/*
+
+Gameboard 
+Create GRID items within gameboard function (3x3)
+
+Might also be applicable for players, just add a feature to it that lets it check for
+1. Function that checks for the marked boxes
+2. Function that checks for wins. loses & ties.
+
+Players
+Create factory function for players that include the following:
+1. Marker function (something that marks the box onclick)
+
+
+*/

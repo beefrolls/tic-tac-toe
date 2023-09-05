a07a6444b6fd9334daa724f683cf59bedbf97210
SupraConstruct <83255856+beefrolls@users.noreply.github.com>
2023-09-05 21:24:22 +0800

    Tested skeleton code

--- code.js
+++ code.js
@@ -13,3 +13,83 @@ Create factory function for players that include the following:
 
 
 */
+
+const gameboard = (() => {
+    let board = [
+        {
+            col1: '0',
+            col2: '0',
+            col3: '0'
+        },
+        {
+            col1: '0',
+            col2: '0',
+            col3: '0'
+        },
+        {
+            col1: '0',
+            col2: '0',
+            col3: '0'
+        }
+    ];
+
+    return {board}
+})();
+
+const game = (() => {
+    let row1 = gameboard.board[0];
+    let row2 = gameboard.board[1];
+    let row3 = gameboard.board[2];
+
+    const markBoard = function(input) {
+        switch (input) {
+            case 'r1c1':
+                row1.col1 = '1';
+                break;
+
+            case 'r1c2':
+                row1.col2 = '1';
+                break;
+
+            case 'r1c3':
+                row1.col3 = '1';
+                break;
+
+            case 'r2c1':
+                row2.col1 = '1';
+                break;
+
+            case 'r2c2':
+                row2.col2 = '1';
+                break;
+
+            case 'r2c3':
+                row2.col3 = '1';
+                break;
+
+            case 'r3c1':
+                row3.col1 = '1';
+                break;
+
+            case 'r3c2':
+                row3.col2 = '1';
+                break;
+
+            case 'r3c3':
+                row3.col3 = '1';
+                break;
+        }
+        console.log(gameboard.board)
+    }
+
+    const checkWin = function() {
+        if ((row1.col1 == '1' && row2.col2 == '1' && row3.col3 == '1') || (row1.col1 == '1' && row1.col2 == '1' && row1.col3 == '1') || (row2.col1 == '1' && row2.col2 == '1' && row2.col3 == '1') || (row3.col1 == '1' && row3.col2 == '1' && row3.col3 == '1')) {
+            console.log('You win!')
+        } else {
+            console.log('You lose!')
+        }
+    }
+
+    return {markBoard, checkWin}
+})();
+
